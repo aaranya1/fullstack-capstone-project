fb96a8fb3136342b398eebb558cc315937cc5021
aaranya1 <aniketaranya1@gmail.com>
2025-12-19 03:32:12 -0500

    modified profile page component for backend

--- giftlink-frontend/src/components/Profile/Profile.js
+++ giftlink-frontend/src/components/Profile/Profile.js
@@ -5,14 +5,14 @@ import {urlConfig} from '../../config';
 import { useAppContext } from '../../context/AuthContext';
 
 const Profile = () => {
-  const [userDetails, setUserDetails] = useState({});
- const [updatedDetails, setUpdatedDetails] = useState({});
- const {setUserName} = useAppContext();
- const [changed, setChanged] = useState("");
+const [userDetails, setUserDetails] = useState({});
+const [updatedDetails, setUpdatedDetails] = useState({});
+const {setUserName} = useAppContext();
+const [changed, setChanged] = useState("");
 
- const [editMode, setEditMode] = useState(false);
-  const navigate = useNavigate();
-  useEffect(() => {
+const [editMode, setEditMode] = useState(false);
+const navigate = useNavigate();
+useEffect(() => {
     const authtoken = sessionStorage.getItem("auth-token");
     if (!authtoken) {
       navigate("/app/login");
@@ -21,22 +21,22 @@ const Profile = () => {
     }
   }, [navigate]);
 
-  const fetchUserProfile = async () => {
+const fetchUserProfile = async () => {
     try {
       const authtoken = sessionStorage.getItem("auth-token");
       const email = sessionStorage.getItem("email");
       const name=sessionStorage.getItem('name');
       if (name || authtoken) {
-                const storedUserDetails = {
-                  name: name,
-                  email:email
-                };
+            const storedUserDetails = {
+                name: name,
+                email:email
+            };
 
-                setUserDetails(storedUserDetails);
-                setUpdatedDetails(storedUserDetails);
-              }
-} catch (error) {
-  console.error(error);
+            setUserDetails(storedUserDetails);
+            setUpdatedDetails(storedUserDetails);
+        }
+} catch (e) {
+  console.error(e);
   // Handle error case
 }
 };
@@ -51,6 +51,7 @@ setUpdatedDetails({
   [e.target.name]: e.target.value,
 });
 };
+
 const handleSubmit = async (e) => {
   e.preventDefault();
 
@@ -65,15 +66,19 @@ const handleSubmit = async (e) => {
 
     const payload = { ...updatedDetails };
     const response = await fetch(`${urlConfig.backendUrl}/api/auth/update`, {
-      //Step 1: Task 1
-      //Step 1: Task 2
-      //Step 1: Task 3
+        method: 'PUT',
+        headers: {
+            "Authorization": `Bearer ${authtoken}`,
+            "Content-Type": "application/json",
+            "Email": email,
+        },
+        body: JSON.stringify(payload),()
     });
 
     if (response.ok) {
       // Update the user details in session storage
-      //Step 1: Task 4
-      //Step 1: Task 5
+      setUserName(updatedDetails.name);
+      sessionStorage.setItem("name", updatedDetails.name);
       setUserDetails(updatedDetails);
       setEditMode(false);
       // Display success message to the user
